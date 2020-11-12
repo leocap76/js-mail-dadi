9652721fab0bff64e1f5404b5cd32db7139df52d
Leonardo Capogna <leonardocapogna@gmail.com>
2020-11-12 16:52:23 +0100

    email non funzionante

---
+++ mail/js/script.js
@@ -0,0 +1,23 @@
+//array lista delle email
+var emailList = ["prova@gmail.com", "alice@gmail.com", "giulia@gmail.com", "alessio@gmail.com", "leo@gmail.com"];
+
+var usermail = prompt ("inserisci la tua email");
+
+var result;
+
+//controllare email: se l'email è inclusa nell'array invia messaggio: accesso consentito,
+//se l'email non è presente, invia messaggio "accesso non consentito"
+
+for (var i = 0; i < emailList.length; i++) {
+  if (usermail == emailList[i]){
+    result = true;
+  } else {
+    result = false;
+  }
+}
+
+if (result === true) {
+  document.getElementById('access').innerHTML = "Accesso Consentito";
+} else {
+  document.getElementById('access').innerHTML = "Accesso non Consentito";
+}
